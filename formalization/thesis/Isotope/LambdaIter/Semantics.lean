8ac72eadd51481d1ad1f61703474318a69e68acc
Jad Ghalayini <jad@tekne.dev>
2026-08-30 16:28:31 +0100

    export lambda-iter semantics modules

---
+++ formalization/thesis/Isotope/LambdaIter/Semantics.lean
@@ -0,0 +1,4 @@
+import Isotope.LambdaIter.Semantics.Model
+import Isotope.LambdaIter.Semantics.Instruction
+import Isotope.LambdaIter.Semantics.Denotation
+import Isotope.LambdaIter.Semantics.Purity
