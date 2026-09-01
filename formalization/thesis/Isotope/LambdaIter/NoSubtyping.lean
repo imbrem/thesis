fc4f05a4411c846127cd918343f56a38c46b0dc2
Jad Ghalayini <jad@tekne.dev>
2026-09-01 14:28:17 +0100

    Prove no-subtyping equality stable under bound renaming

--- formalization/thesis/Isotope/LambdaIter/NoSubtyping.lean
+++ formalization/thesis/Isotope/LambdaIter/NoSubtyping.lean
@@ -1,4 +1,6 @@
 import Isotope.LambdaIter.NoSubtyping.Equiv
 import Isotope.LambdaIter.NoSubtyping.Metatheory
+import Isotope.LambdaIter.NoSubtyping.Metatheory.Syntax
+import Isotope.LambdaIter.NoSubtyping.Metatheory.EquivSubst
 
 /-! The parallel, syntax-directed lambda-iter experiment without subtyping. -/
