44b6cfea996905d75277c3b40d249b7e28ae47d7
Jad Ghalayini <jad@tekne.dev>
2026-09-01 14:14:07 +0100

    add coercion-free categorical semantic kernel

--- formalization/thesis/Isotope/LambdaIter/NoSubtyping.lean
+++ formalization/thesis/Isotope/LambdaIter/NoSubtyping.lean
@@ -1,4 +1,5 @@
 import Isotope.LambdaIter.NoSubtyping.Equiv
 import Isotope.LambdaIter.NoSubtyping.Metatheory
+import Isotope.LambdaIter.NoSubtyping.Semantics.Categorical
 
 /-! The parallel, syntax-directed lambda-iter experiment without subtyping. -/
